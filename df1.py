8ab6fc88c769ddb559449b4f40048765b22681ba
Your Name <hardik.sondhi2022@vitstudent.ac.in>
2025-10-07 11:54:12 +0530

    input-constrained

--- df1.py
+++ df1.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import cv2
 import pyttsx3
+import speech_recognition as sr
 import threading
 
 # Start webcam
@@ -33,25 +34,37 @@ previous_objects = set()
 
 # Thread-safe TTS speaker
 def speak(text):
-    def _speak():
-        engine.say(text)
-        engine.runAndWait()
-    threading.Thread(target=_speak, daemon=True).start()
+    engine.say(text)
+    engine.runAndWait()
 
-while True:
+speak("What do you want me to look for?")
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say something!")
+    r.adjust_for_ambient_noise(source)  # Adjust for background noise
+    audio = r.listen(source)
+
+text = r.recognize_google(audio)
+print("Google Speech Recognition thinks you said: " + text)
+flag=0
+while (flag==0):
     success, img = cap.read()
     results = model(img, stream=True)
 
     # registry of what’s in the current frame
     current_objects.clear()
-
     for r in results:
         boxes = r.boxes
         for box in boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
             cls = int(box.cls[0])
             label = classNames[cls]
-
+            if(label==text):
+                print("I can see "+text+" now turning off the application")
+                flag=1
+                speak("I can see laptop now turning off the application")
             # add to registry
             current_objects.add(label)
 
@@ -60,15 +73,15 @@ while True:
             cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX,
                         0.7, (0, 255, 0), 2)
 
-    # If registry changed → announce it
-    if current_objects != previous_objects:
-        if current_objects:
-            msg = "I can see " + ", ".join(current_objects)
-        else:
-            msg = "I see nothing now"
-        print("Speaking:", msg)
-        speak(msg)
-        previous_objects = current_objects.copy()
+    # Speaks for all objects
+    # if current_objects != previous_objects:
+    #     if current_objects:
+    #         msg = "I can see " + ", ".join(current_objects)
+    #     else:
+    #         msg = "I see nothing now"
+    #     print("Speaking:", msg)
+    #     speak(msg)
+    #     previous_objects = current_objects.copy()
 
     cv2.imshow("Webcam", img)
     if cv2.waitKey(1) == ord("q"):
